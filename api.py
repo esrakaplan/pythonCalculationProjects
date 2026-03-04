44e737f25c61388978620938750fd9e939751649
esrakaplan <ess.kaplan@gmail.com>
2026-03-04 15:27:15 +0300

    exam results

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
-# rag_system.py
-
-import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
+app = FastAPI()
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-# Mini knowledge base
 documents = [
     "Machine learning is a field of AI where models learn from data.",
     "Supervised learning uses labeled data.",
@@ -16,27 +16,33 @@ documents = [
     "Deep learning uses neural networks."
 ]
 
-# Document embeddings
 doc_embeddings = model.encode(documents)
 
 
-def retrieve_answer(question):
-    question_embedding = model.encode([question])
+class Query(BaseModel):
+    question: str
 
+
+@app.post("/ask")
+def ask_question(query: Query):
+    question_embedding = model.encode([query.question])
     similarities = cosine_similarity(question_embedding, doc_embeddings)
     best_index = np.argmax(similarities)
 
     return {
-        "question": question,
+        "question": query.question,
         "best_match": documents[best_index],
         "similarity_score": float(similarities[0][best_index])
     }
 
 
-# Test
-if __name__ == "__main__":
-    result = retrieve_answer("Explain supervised models")
-    print(result)
-    result = retrieve_answer("Explain Machine learning")
-    print(result)
 
+"""
+uvicorn api:app --reload
+
+
+postman request POST 'http://127.0.0.1:8000/ask' \
+  --header 'Content-Type: application/json' \
+  --body '{
+  "question": "What is supervised learning?"
+}'"""
